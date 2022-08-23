130d346fe2be414ad4baca8ad9639fa21e82dea1
yigit <yigitozdamar@hotmail.com>
2022-08-23 14:01:29 +0300

    .

--- AsyncImageSwiftUI/ContentView.swift
+++ AsyncImageSwiftUI/ContentView.swift
@@ -9,8 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            VStack{
+                AsyncImage(url: URL(string: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/Earth_poster_large.jpg/1600px-Earth_poster_large.jpg?20080517082911") ) { image in
+                    image.resizable().frame(width: 300, height: 300, alignment: .center)
+                } placeholder: {
+                    ProgressView()
+                }
+
+                
+                List(superHeroArray){
+                    superhero in
+                    Text(superhero.name).font(.title2)
+                }.navigationTitle(Text("Superhero Book"))
+            }
+            
+        }
     }
 }
 
